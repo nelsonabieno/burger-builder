56de1842b1a02697782693f7673a073806d51adb
nelsonabieno <rotimi.nelson@andela.com>
2018-01-26 16:17:50 +0100

    create order summary modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -88,13 +90,16 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-         <Burger ingredients= {this.state.ingredients}/>
-         <BuildControls
-          ingredientsAdded = {this.addIngredientHandler}
-          ingredientsRemoved = {this.removeIngredientHandler}
-          disabled = {disabledInfo}
-          purchasable = {this.state.purchasable}
-          price = {this.state.totalPrice}/>
+        <Modal>
+          <OrderSummary ingredients = {this.state.ingredients}/>
+        </Modal>
+        <Burger ingredients= {this.state.ingredients}/>
+        <BuildControls
+        ingredientsAdded = {this.addIngredientHandler}
+        ingredientsRemoved = {this.removeIngredientHandler}
+        disabled = {disabledInfo}
+        purchasable = {this.state.purchasable}
+        price = {this.state.totalPrice}/>
       </Aux>
     );
   }
