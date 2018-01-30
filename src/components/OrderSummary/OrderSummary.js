1440b037bf0d6b895cebb846c4f8f01628aaea4d
nelsonabieno <rotimi.nelson@andela.com>
2018-01-30 15:56:36 +0100

    add total price to the order summary

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -20,6 +20,7 @@ const orderSummary = (props) => {
       <ul>
         {ingredientSummary}
       </ul>
+      <p><strong> Total Price: {props.price.toFixed(2)} </strong> </p>
       <p> Continue to Checkout? </p>
       <Button clicked= {props.purchaseCancelled} btnType= "Danger"> CANCEL </Button>
       <Button clicked = {props.purchaseContinued} btnType="Success"> CONTINUE</Button>
