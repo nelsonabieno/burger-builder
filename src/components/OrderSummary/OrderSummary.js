8e0dde2b96362570dc02da1d5cecae541a500e60
nelsonabieno <rotimi.nelson@andela.com>
2018-01-30 13:25:34 +0100

    implement button component in order summary

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import Button from '../UI/Button/Button';
 
 const orderSummary = (props) => {
 
@@ -20,6 +21,8 @@ const orderSummary = (props) => {
         {ingredientSummary}
       </ul>
       <p> Continue to Checkout? </p>
+      <Button clicked= {props.purchaseCancelled} btnType= "Danger"> CANCEL </Button>
+      <Button clicked = {props.purchaseContinued} btnType="Success"> CONTINUE</Button>
     </Aux>
   );
 };
